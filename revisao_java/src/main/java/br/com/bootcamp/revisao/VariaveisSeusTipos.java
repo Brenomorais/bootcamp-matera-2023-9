4b99a7732b2e492293df95cd531df7380bc6a707
breno <breno.lopes.morais@gmail.com>
2023-09-16 17:31:49 -0300

    Um pouco sobre variaveis e seus tipos.

---
+++ revisao_java/src/main/java/br/com/bootcamp/revisao/VariaveisSeusTipos.java
@@ -0,0 +1,18 @@
+package br.com.bootcamp.revisao;
+
+public class VariaveisSeusTipos {
+    public static void main(String[] args) {
+        int idade = 25;
+
+        double altura = 1.78;
+
+        String nome = "breno";
+
+        boolean gostaDeChocolate = true;
+
+        System.out.println("idade:" + idade);
+        System.out.println("altura:" + altura);
+        System.out.println("nome:" + nome);
+        System.out.println("Gosta de chocolate:" + gostaDeChocolate);
+    }
+}
