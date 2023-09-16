efdc5cf38ec0689fb3ffa522f7b1e0e850e22a5b
breno <breno.lopes.morais@gmail.com>
2023-09-16 17:36:35 -0300

    Tomando decisoes com IF e Else.

---
+++ revisao_java/src/main/java/br/com/bootcamp/revisao/TomandoDecisoesComIfElse.java
@@ -0,0 +1,13 @@
+package br.com.bootcamp.revisao;
+
+public class TomandoDecisoesComIfElse {
+    public static void main(String[] args) {
+        int temperatura = 15;
+
+        if(temperatura < 20){
+            System.out.println("Melhor vestir um suéter!");
+        }else{
+            System.out.println("Uma camiste está ótima!");
+        }
+    }
+}
