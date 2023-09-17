c51d5847a23fb80800e5db5aa2ff8c36e2c43895
breno <breno.lopes.morais@gmail.com>
2023-09-17 16:43:55 -0300

    Projeto inicial gerenciador de carros criado com dependencia spring web, gerado pelo spring initializr.

---
+++ gerenciadorDeCarros/src/test/java/com/breno/bootcamp/gerenciadorDeCarros/GerenciadorDeCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breno.bootcamp.gerenciadorDeCarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
